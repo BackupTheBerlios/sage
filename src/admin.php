52859e2b7ee6a13c56d0eb609a88c415dc7b42b5
lou <lou>
2002-05-15 18:14:11 +0000

    Noch mehr dateien (Tools)

---
+++ src/admin.php
@@ -0,0 +1,20 @@
+<?PHP
+/*Variablen Deklaration*/
+
+//Page-Name
+$PageName="SAGE";
+
+/*Der Header wird mit eingebunden*/
+require "headeradmin.inc";
+?>
+
+<?PHP
+/*Ab hier der Hauptteil*/
+?>
+
+
+
+<?PHP
+/*Der Rumpf wird mit eingebunden*/
+require "futteradmin.inc";
+?>
