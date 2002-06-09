fa9d24a791884ad28431bab32b11d5be485b11f9
Baldimann <Baldimann>
2002-06-09 14:07:52 +0000

    Zugriff auf die Datenbank & das Verschieben zwischen der Werte zwischen den Listen ist möglich.

--- src/NeueGruppeEinrichten.php
+++ src/NeueGruppeEinrichten.php
@@ -1,8 +1,10 @@
-<?PHP
+<?php
+          
+      include_once("zwei_listen.inc.php");
+      
+echo"
+	<form method='POST' action='NeueGruppeEinrichten.php'>
 
-	echo"	
-	<form method='POST' action='UserEinrichtenCheck.php'>
-		
 		<h2>Neue Gruppe einrichten</h2>
 		<hr>
 		<table border='0'>
@@ -10,72 +12,72 @@
 				<td>
 				Gruppenname:
 				</td>
-				
+
 				<td>
 				<input type='text' name='GruppenName'>
 				</td>
 			</tr>
 		</table>
-		
+
 		<table border='0'>
 			<tr>
 				<td width='240'>
 				Alle Rechte:
 				</td>
-				
+
 				<td>
 				</td>
-				
+
 				<td width='240'>
 				Ausgewählte Rechte:
 				</td>
 			</tr>
-			
+
 			<tr>
 				<td width='240'>
-				<select name='AlleRechte' size='10' >
-				<option>User einrichten</option>
-				<option>User löschen</option>
-				<option>Neue Gruppe einrichten</option>
-				</select>
+";
+
+				links();
+
+echo"
 				</td>
 
 				<td>
-				<input type='button' name='LinksRechts' value='>>'>
+				<input type='submit' name='LinksRechts' value='>>'>
 				<br>
-				<input type='button' name='RechtsLinks' value='<<'>
+				<input type='submit' name='RechtsLinks' value='<<'>
 				</td>
-		   
+
 				<td width='240'>
-				<select name='AlleRechte' size='10'>
-				<option>User einrichten</option>
-				</select>
+";
+
+				rechts($LinksRechts, $RechtsLinks);
+
+echo"
 				</td>
 		    </tr>
-			
+
 		</table>
-		
+
 		<hr>
 		<br>
-		
+
 		<table>
 		<tr>
-			<td>		
-			<input type='submit' value='     OK     ' name='OK' >
-			
+			<td>
+			<input type='submit' value='OK' name='OK' style='WIDTH:90'>
+
 			</td>
-			
+
 			<td width='20'>
 			</td>
-			
+
 			<td>
 			<input type='button' value='Abbrechen' name='Abbrechen' >
 			</td>
 		</tr>
-	<input type='hidden' name='gesendet' value='1'>
 	</form>
-	";
-
+";
 
 
 ?>
