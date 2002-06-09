0a54f4a4a7c92152453163b501aef6016bd4bf76
p7haas <p7haas>
2002-06-09 14:51:49 +0000

    Browser

---
+++ src/browser.php
@@ -0,0 +1,107 @@
+<?php
+require_once("inc/functions.inc.php");
+require_once("inc/path.inc.php");
+require_once("inc/fehlerausgabe.inc.php");
+
+function printHeader()
+{
+    echo <<<EOF
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="table-layout:fixed">
+    <tr>
+        <td bgcolor="#CCCCCC" nowrap="nowrap" valign="top">
+            Name
+        </td>
+        <td bgcolor="#CCCCCC" nowrap="nowrap" valign="top">
+            Besitzer
+        </td>
+        <td bgcolor="#CCCCCC" nowrap="nowrap" valign="top">
+            Beschreibung
+        </td>
+        <td bgcolor="#CCCCCC" nowrap="nowrap" valign="top">
+            erstellt am
+        </td>
+    </tr>
+EOF;
+}
+
+
+
+
+function printDirectoryEntry(&$path, $isFile)
+{
+    if ($isFile) {
+
+
+
+
+    } else {
+        $me = $_SERVER["PHP_SELF"];
+        echo ("<tr>");
+        echo("<td><img src=\"icons/dir.gif\" alt=\"[dir]\" />");
+
+        echo("<a href=\"$me?path=$path->pathname\">$path->pathname</a></td>\n");
+        echo("<td>$path->loginname</td>");
+        echo("<td>$path->description</td>");
+        echo("<td>$path->insert_at</td>");
+        echo("</tr>");
+    }
+}
+
+function printFooter()
+{
+    echo ("</table>");
+}
+
+$PageName = "Browser";
+require("inc/header.inc.php");
+require("inc/leftnav.inc.php");
+
+
+
+$_SESSION["path"] = $_REQUEST["path"];
+
+if (!isset($_SESSION["path"])) {
+    $_SESSION["path"] = "/";
+}
+
+//  Verzeichniseintrag holen
+$path = new Path;
+if (!$path->selectByName($_SESSION["path"])) {
+    fehlerausgabe("Verzeichnis existiert nicht!");
+    die();
+}
+
+echo ("Verzeichnis: ".$path->pathname."<p />");
+
+// ACL für das Verzeichnis holen
+$user = $_SESSION["user"];
+$acl = $user->getACLByPath($path->pathname);
+if ($acl->read_path != "1") {
+    fehlerausgabe("Zugriff auf $path->pathname verweigert");
+    die();
+}
+
+printHeader();
+// Verzeichnisse listen
+$parent = new Path;
+if ($parent->selectById($path->path_id_parent)) {
+    printDirectoryEntry($parent, false);
+}
+
+$pathlist = new PathList;
+$pathlist->selectByParentId($path->path_id);
+for ($i = 0; $i < count($pathlist->list); $i++) {
+    $acl = $_SESSION["user"]->getACLByPath($pathlist->list[$i]->pathname);
+    if ($acl->read_path) {
+        $currentPath = $pathlist->list[$i];
+        printDirectoryEntry($currentPath, false);
+    }
+}
+printFooter();
+?>
+
+
+<?php
+require("inc/footer.inc.php");
+?>
---
+++ src/inc/path.inc.php
@@ -0,0 +1,93 @@
+<?php
+require_once("mysql_class.inc.php");
+
+
+class Path
+{
+    var $path_id;
+    var $loginname;
+    var $pathname;
+    var $description;
+    var $insert_at;
+    var $modified_at;
+    var $path_id_parent;
+
+
+    function selectByID($id)
+    {
+        $dbq = new DB;
+        $dbq->db_connect();
+
+        $query = "SELECT path_id, loginname, pathname, description, insert_at, modified_at, path_id_parent
+                  FROM sage_path
+                  WHERE path_id = $id";
+
+        $result = $dbq->db_select($query);
+        if (count($result) == 0) return false;
+
+        $this->initializeFromRow($result[0]);
+
+        return true;
+    }
+
+    function selectByName($name)
+    {
+        $dbq = new DB;
+        $dbq->db_connect();
+
+        $query = "SELECT path_id, loginname, pathname, description, insert_at, modified_at, path_id_parent
+                  FROM sage_path
+                  WHERE pathname = '$name'";
+
+        $result = $dbq->db_select($query);
+        if (count($result) == 0) return false;
+
+        $this->initializeFromRow($result[0]);
+
+        return true;
+    }
+
+
+    function initializeFromRow($row)
+    {
+        $this->path_id          = $row->path_id;
+        $this->loginname        = $row->loginname;
+        $this->pathname         = $row->pathname;
+        $this->description      = $row->description;
+        $this->insert_at        = $row->insert_at;
+        $this->modified_at      = $row->modified_at;
+        $this->path_id_parent   = $row->path_id_parent;
+    }
+
+}
+
+class PathList {
+    var $list = 0;
+
+    function PathList()
+    {
+        $this->list = array();
+    }
+
+    function selectByParentId($id)
+    {
+        $dbq = new DB;
+        $dbq->db_connect();
+
+        $query = "SELECT path_id, loginname, pathname, description, insert_at, modified_at, path_id_parent
+                  FROM sage_path
+                  WHERE path_id_parent = $id";
+
+        $paths = $dbq->db_select($query);
+
+        for ($i = 0; $i < count($paths); $i++) {
+            $path = new Path;
+            $path->initializeFromRow($paths[$i]);
+            $this->list[] = $path;
+        }
+
+        return true;
+    }
+}
+
+?>
