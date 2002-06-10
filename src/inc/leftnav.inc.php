04ffe993dd51a97a3adc7336942171c0a9b81e36
lou <lou>
2002-06-10 21:50:00 +0000

    leftnav den Anforderungen angepasst
    link_array:
    In dieser Datei befinden sich fuer jedes Modul die linkeintraege
    wird ein neues Modul hinzugefuegt so muss der passende link hier
    eingefuegt werden!

--- src/inc/leftnav.inc.php
+++ src/inc/leftnav.inc.php
@@ -4,11 +4,38 @@
       <div align="center"><u>Menü</u></div><br>
 
       <?php
-           require_once "inc/verzeichnis.inc.php";
-           $obergrenze=count($linkArray);
-           for($a=0;$a<$obergrenze;$a++)
-               echo "$linkArray[$a]";
+           	require_once ("module.inc.php");
+		   	require_once ("link_array.inc.php");
+			require_once("acl.inc.php");
+			require_once("user.inc.php");
+
+			function getaccess(&$test){
+				if($test->acl_id!=0)
+					return true;
+				return false;
+			}
+
+           	$modul=new ModuleList;
+           	$modul->selectAll();
+		   	$anz=count($modul->list);
+		   	for($i=0;$i<$anz;$i++){	
+				$acl=new ACL;
+		   		$acl=@$_SESSION["user"]->getACLByPath($modul->list[$i]->pathname);
+				if(getaccess($acl))
+					echo $linkRechteListe[$modul->list[$i]->pathname];
+			}
       ?>
+
+
       <div align="center"><a href="main.php">Home</a></div><br />
     </td>
     <td nowrap style="width:80%">
+
+    <?php
+       /*
+       require_once "inc/verzeichnis.inc.php";
+       $obergrenze=count($linkArray);
+       for($a=0;$a<$obergrenze;$a++)
+           echo "$linkArray[$a]";
+       */
+    ?>
---
+++ src/inc/link_array.inc.php
@@ -0,0 +1,8 @@
+<?php
+	$linkRechteListe=array(
+         CALENDAR=>"<div align=\"center\"><a href=\"calendar.php\">Kalender</a></div><br/>",
+         MAIL=>"<div align=\"center\"><a href=\"mail.php\">Mail</a></div><br/>",
+         ADMIN=>"<div align=\"center\"><a href=\"admin.php\">Admin Tools</a></div><br/>"
+    );
+?>
+
