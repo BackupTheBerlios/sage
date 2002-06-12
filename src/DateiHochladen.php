1eab5713aed0e29de49d00abfa8b1fe3b3d83875
Baldimann <Baldimann>
2002-06-12 18:33:26 +0000

    Dateiauswahlbutton hinzugefügt

--- src/DateiHochladen.php
+++ src/DateiHochladen.php
@@ -1,67 +1,63 @@
-<?PHP
-echo"
+<?php
+
+echo("
 <h2>Datei hochladen</h2>
 
 <form name='DateiSuchen' method='post' action='collect.php'>
-<table border = 0> 
+<table border = 0>
 	<tr>
-		<td width='120'>
+		<td width='100'>
 		<b>Lokale Datei:</b>
 		</td>
 		
-		<td width='120'>
-		<input name='LokaleDatei' size='25'> 
-		</td>
-		
-		<td width='120' align='right'>
-		<input type='button'  value='Durchsuchen' style='WIDTH:90' >
+		<td width='280'>
+		<input type='file' name='FileUpload' size='30' accept='Mime-Type' />
 		</td>
 	</tr>
 </table>
 </form>	
 
 <form name='DateiInfo' method='post' action=''>
-<table border = 0> 
+<table border = 0>
 	<tr>
-		<td width='120'>
-		<b>Name:*</b>
+		<td width='100'>
+		<b>Name:</b>
 		</td>
 		
-		<td width ='240'>
-		<input name='DateiName' size='47'> 
+		<td width ='280'>
+		<input name='FileName' size='52' />
 		</td>
 	</tr>
-	
+
 	<tr>
-		<td width='120' valign='top'>
-		<b>Beschreibung:*</b>
+		<td width='100' valign='top'>
+		<b>Beschreibung:</b>
 		</td>
-		
-		<td width ='240'>
-		<textarea cols='36' rows='10' name='Beschreibung'>
-		</textarea> 
-		</td>	
-		
+
+		<td width ='280'>
+		<textarea cols='39' rows='10' name='FileDescription'>
+		</textarea>
+		</td>
+
 	</tr>
 </table>
 
 
-<table border = 0> 
+<table border = 0>
 	<tr>
-		<td width='120'>
-		<small>* optionale Felder</small>
+		<td width='100'>
+                </td>
+
+		<td width='140'>
+		<input type='submit'  name='FileOK' value='OK' style='WIDTH:90' />
 		</td>
-		
-		<td width='120'>
-		<input type='submit'  value='OK' style='WIDTH:90' >
-		</td>
-		
-		<td width='120' align='left'>
-		<input type='submit'  value='Abbrechen' style='WIDTH:90' >
+
+		<td width='140' align='left'>
+		<input type='button' name='FileCancle' value='Abbrechen' style='WIDTH:90' />
 		</td>
 	</tr>
 </table>
 </form>
-";
+");
 
 ?>
