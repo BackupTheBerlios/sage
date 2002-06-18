978a1939c0c76e365f945faa461f15491d189ff8
lou <lou>
2002-06-18 15:50:04 +0000

    Um den Link Hilfe ergaenzt

--- src/inc/link_array.inc.php
+++ src/inc/link_array.inc.php
@@ -2,7 +2,8 @@
 	$linkRechteListe=array(
          CALENDAR=>"<div align=\"center\"><a href=\"calendar.php\">Kalender</a></div><br/>",
          MAIL=>"<div align=\"center\"><a href=\"mail.php\">Mail</a></div><br/>",
-         ADMIN=>"<div align=\"center\"><a href=\"admin.php\">Admin Tools</a></div><br/>"
+         ADMIN=>"<div align=\"center\"><a href=\"admin.php\">Admin Tools</a></div><br/>",
+	 HILFE=>"<div align=\"center\"><a href=\"hilfe.php\">Hilfe</a></div><br/>",
     );
 ?>
 
