08f9f66c3049c8e4a9825dd1b774b35333310ac7
kairu <kairu>
2002-06-19 22:50:43 +0000

    Variablen $htgroup und $htpasswd hinzugefuegt

--- src/inc/config.inc.php
+++ src/inc/config.inc.php
@@ -3,4 +3,7 @@ $sage_version = "0.0.1";
 
 $sage_data_dir = "/data/sage";
 
+$htgroup = $sage_data_dir."/.htgroup";        // Dateiname der htaccess-Gruppendatei
+$htpasswd = $sage_data_dir."/.htpasswd";      // Dateiname der htaccess-Passwortdatei
+
 ?>
