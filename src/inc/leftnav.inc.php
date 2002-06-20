4b1d44d81c8c67417c35be377d3ec93f17849fde
lou <lou>
2002-06-20 19:36:34 +0000

    Das Lyout ein wenig geändert ...

--- src/inc/leftnav.inc.php
+++ src/inc/leftnav.inc.php
@@ -1,24 +1,24 @@
 <table width="100%" border="0" cellspacing="5" cellpadding="10" style="table-layout:fixed">
   <tr>  
     <td bgcolor="#CCCCCC" nowrap valign="top" style="width:20%">
-      <div align="center"><u>Menü</u></div><br>
+	<div align="center"><H2>Menü</H2></div>
 
       <?php
-        require_once ("module.inc.php");
-		require_once ("link_array.inc.php");
-		require_once("acl.inc.php");
-		require_once("user.inc.php");
+           	require_once ("module.inc.php");
+		   	require_once ("link_array.inc.php");
+			require_once("acl.inc.php");
+			require_once("user.inc.php");
 
-		function getaccess(&$test){
-			if($test->acl_id!=0)
-				return true;
-			return false;
-		}
+			function getaccess(&$test){
+				if($test->acl_id!=0)
+					return true;
+				return false;
+			}
 
            	$modul=new ModuleList;
            	$modul->selectAll();
 		   	$anz=count($modul->list);
-		   	for($i=0;$i<$anz;$i++){
+		   	for($i=0;$i<$anz;$i++){	
 				$acl=new ACL;
 		   		$acl=@$_SESSION["user"]->getACLByPath($modul->list[$i]->pathname);
 				if(getaccess($acl))
@@ -31,11 +31,11 @@
     </td>
     <td nowrap style="width:80%">
 
-
-
-
-
-
-
-
- 
+    <?php
+       /*
+       require_once "inc/verzeichnis.inc.php";
+       $obergrenze=count($linkArray);
+       for($a=0;$a<$obergrenze;$a++)
+           echo "$linkArray[$a]";
+       */
+    ?>
