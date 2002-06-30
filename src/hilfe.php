5a294d0f2514e1e3ffb64d65695c2af365b0653d
p7haas <p7haas>
2002-06-30 19:39:09 +0000

    Dateiformate global auf Unix umgestellt

--- src/hilfe.php
+++ src/hilfe.php
@@ -9,11 +9,11 @@ require "inc/header.inc.php";
 require "inc/leftnav.inc.php";
 ?>
 
-<?php    
+<?php
 	echo "<div align=\"center\"><H1>SAGE-HILFE</H1></div><br/>";
 	require_once ("inc/module.inc.php");
     require_once("inc/help.inc.php");
-	
+
     $modul=new ModuleList;
     $modul->selectAll();
     $anz=count($modul->list);
--- src/inc/leftnav.inc.php
+++ src/inc/leftnav.inc.php
@@ -4,30 +4,30 @@
     <td bgcolor="#274E9C" nowrap="nowrap" valign="top" style="width:20%">
 	<div align="center"><h2><font color="#CAA778">Men&uuml;</font></h2></div>
 
-      <?php
-           	require_once ("module.inc.php");
-		   	require_once ("link_array.inc.php");
-			require_once("acl.inc.php");
-			require_once("user.inc.php");
+<?php
+    require_once ("module.inc.php");
+    require_once ("link_array.inc.php");
+    require_once("acl.inc.php");
+    require_once("user.inc.php");
 
-			function getaccess(&$test){
-				if($test->acl_id!=0)
-					return true;
-				return false;
-			}
+	function getaccess(&$test){
+	    if($test->acl_id!=0)
+	        return true;
+	        return false;
+	}
 
-            echo("<font color=\"#CAA778\">");
-           	$modul=new ModuleList;
-           	$modul->selectAll();
-		   	$anz=count($modul->list);
-		   	for($i=0;$i<$anz;$i++){
-				$acl=new ACL;
-		   		$acl=@$_SESSION["user"]->getACLByPath($modul->list[$i]->pathname);
-				if(getaccess($acl))
-					echo $linkRechteListe[$modul->list[$i]->pathname];
-			}
-			echo("</font>");
-      ?>
+    echo("<font color=\"#CAA778\">");
+    $modul=new ModuleList;
+    $modul->selectAll();
+	$anz=count($modul->list);
+	for($i=0;$i<$anz;$i++){
+        $acl=new ACL;
+        $acl=@$_SESSION["user"]->getACLByPath($modul->list[$i]->pathname);
+        if(getaccess($acl))
+            echo $linkRechteListe[$modul->list[$i]->pathname];
+    }
+    echo("</font>");
+?>
 
       <div align="center"><a href="profile.php" style="color: #CAA778;">Benutzerprofil</a></div><br />
       <div align="center"><a href="index.php" style="color: #CAA778;">Home</a></div><br />
@@ -35,11 +35,11 @@
     <!--- <td nowrap="nowrap" style="width:80%" valign="top"> -->
     <td style="width:80%" valign="top">
 
-    <?php
-       /*
-       require_once "inc/verzeichnis.inc.php";
-       $obergrenze=count($linkArray);
-       for($a=0;$a<$obergrenze;$a++)
-           echo "$linkArray[$a]";
-       */
-    ?>
+<?php
+    /*
+    require_once "inc/verzeichnis.inc.php";
+    $obergrenze=count($linkArray);
+    for($a=0;$a<$obergrenze;$a++)
+        echo "$linkArray[$a]";
+    */
+?>
